1e617140c027395e4f7e7608b94e8a463f10a04b
Raul Vargas Osorio <vargas.r.18@gmail.com>
2024-08-25 15:24:29 -0400

    ejecucion en hilos

--- test.py
+++ test.py
@@ -1,21 +1,37 @@
 import RPi.GPIO as GPIO
-import time
 import redis
+import threading
 
+
+# Start Setup
 rd = redis.Redis('localhost')
-
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.IN)
-count = 0
-old_data = 0
+pins = [3, 5, 7, 11, 13, 15, 19, 21, 23, 29]
+for pin in pins:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+counters = {
+    pin: 0 for pin in pins
+}
+old_data = {
+    pin: 0 for pin in pins
+}
 
-print("Count: {}".format(count))
-for x in range(100):
-    input = GPIO.input(3)
-    if input != old_data:
-        old_data = input
-        if input:
-            count += 1
-            rd.set('{}'.format('in_3'), count)
-print("Count: {}".format(count))
+# Function to count pulses
+def count_pulses(index):
+    global old_data
+    global counters
+    while True:
+        input = GPIO.input(index)
+        if input != old_data[index]:
+            old_data[index] = input
+            if input == GPIO.HIGH:
+                counters[index] += 1
+                rd.set('{}'.format('input_{}'.format(index)), counters[index])
+
+# Start Threads
+threads = []
+for pin in pins:
+    threads.append(threading.Thread(target=count_pulses, args=(pin,)))
+    threads[-1].start()
+
 
