6d223d1cde93a255cfd55762abfc9f120c9ac322
Raul Vargas Osorio <vargas.r.18@gmail.com>
2024-08-26 22:05:32 -0400

    fixes prueba

--- prueba.py
+++ prueba.py
@@ -1,9 +1,15 @@
 from gpiozero import Button
 import pines
 
-counter = 0
-button = Button(pines.pins[0])
-button.when_pressed = lambda: globals().update(counter=counter+1)
-button.when_released = lambda: print(counter)
 
-pause()
+counter = 0
+def increment_counter():
+    global counter
+    counter += 1
+
+button = Button(pines.pins[0])
+
+while True:
+    button.when_pressed = increment_counter
+    button.when_released = print(counter)
+
