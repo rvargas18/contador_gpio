7171986f8b224dde802d92524a3fb495ba230668
Raul Vargas <vargas.r.18@gmail.com>
2024-10-26 15:27:19 +0000

    ultimas pruebas realizadas

--- prueba_gpiozero.py
+++ prueba_gpiozero.py
@@ -1,25 +1,44 @@
 from gpiozero import Button
 from signal import pause
-import pines
+import threading
+import cronus.beat as beat
+from datetime import datetime as dt
+import redis
+
+# DB redis
+r = redis.Redis('localhost')
 
 # Inicializa el contador
+pin = 17
 counter = 0
-
+counter_2 = 0
 # Define una función para incrementar el contador
 def increment_counter():
     global counter
     counter += 1
+    r.set('counter_17', counter)
 
 # Define una función para imprimir el contador
+# frecuencia = 1. / 60.    # en Hz
+# beat.set_rate(frecuencia)
+
 def print_counter():
-    print(f"Contador: {counter}")
+    global counter_2
+    while beat.true():
+        count_d = counter - counter_2
+        print(f"{dt.now()} | Contador: {counter} - {count_d}")
+        counter_2 = counter
+        beat.sleep()  # Espera 60 segundos antes de imprimir de nuevo
 
 # Configura el botón con un tiempo de debounce de 0.2 segundos (ajustable según sea necesario)
-button = Button(18, bounce_time=0.02)
+button = Button(pin, bounce_time=0)
 
 # Asigna las funciones a los eventos del botón
 button.when_pressed = increment_counter
-button.when_released = print_counter
+#button.when_released = increment_counter
+
+# Inicia el hilo para imprimir el contador
+# threading.Thread(target=print_counter, daemon=True).start()
 
 # Mantén el programa en ejecución
 pause()
--- read_redis.py
+++ read_redis.py
@@ -1,11 +1,11 @@
 import redis
 import cronus.beat as beat
 from datetime import datetime as dt
-import pines
+# import pines
 
 
 r = redis.Redis('localhost')
-pins = pines.pins
+# pins = pines.pins
 frecuencia = 1. / 60.    # en Hz
 i = 0
 beat.set_rate(frecuencia)
@@ -13,8 +13,8 @@ while beat.true():
     print("\nCiclo #{}".format(i))
     now = dt.now()
     print(now)
-    for pin in pins:
-        count = r.get('input_{}'.format(pin))
+    for pin in [17]:
+        count = r.get('counter_{}'.format(pin))
         _count = int(count) if count else 0
         print('Count pin {}: {}'.format(pin, _count))
     i += 1
